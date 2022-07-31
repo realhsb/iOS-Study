3b2c17fb14418fdf7b43d6a77467e07d6cc43278
realhsb <realhsb@gmail.com>
2022-07-31 16:07:01 +0900

    Scene-Trans02

---
+++ Swift-Basic/Scene-Trans02/Scene-Trans02/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  Scene-Trans02
+//
+//  Created by 한수빈 on 2022/07/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func moveByNavi(_ sender: Any) {
+        // 두번째 뷰 컨트롤러 인스턴스를 가져온다.
+        guard let uvc = self.storyboard?.instantiateViewController(withIdentifier: "SecondVC") else {
+            return
+        }
+        
+        // 화면을 전환한다.
+        self.navigationController?.pushViewController(uvc, animated: true)
+    }
+    
+    
+    @IBAction func movePresent(_ sender: Any) {
+        // 두 번째 뷰 컨트롤러 인스턴스를 가져온다.
+        guard let uvc = self.storyboard?.instantiateViewController(withIdentifier: "SecondVC") else {
+            return
+        }
+        // 화면을 전환한다.
+        self.present(uvc, animated: true)
+    }
+}
+
---
+++ Swift-Basic/Scene-Trans02/Scene-Trans02Tests/Scene_Trans02Tests.swift
@@ -0,0 +1,33 @@
+//
+//  Scene_Trans02Tests.swift
+//  Scene-Trans02Tests
+//
+//  Created by 한수빈 on 2022/07/25.
+//
+
+import XCTest
+@testable import Scene_Trans02
+
+class Scene_Trans02Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
