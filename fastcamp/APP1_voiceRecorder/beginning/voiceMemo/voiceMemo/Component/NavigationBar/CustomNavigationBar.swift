f3b16bf7e109af82d31ea9940bb7785e240a471a
realhsb <realhsb@gmail.com>
2023-11-14 00:53:17 +0900

    ✨ Custom Navigation bar 생성

--- fastcamp/APP1_voiceRecorder/beginning/voiceMemo/voiceMemo/Component/NavigationBar/CustomNavigationBar.swift
+++ fastcamp/APP1_voiceRecorder/beginning/voiceMemo/voiceMemo/Component/NavigationBar/CustomNavigationBar.swift
@@ -6,9 +6,58 @@
 import SwiftUI
 
 struct CustomNavigationBar: View {
+	let isDisplayLeftBtn: Bool
+	let isDisplayRightBtn: Bool
+    
+	let leftBtnAction: () -> Void		// 액션을 받아와서 전달해줌
+	let rightBtnAction: () -> Void
+	
+	let rightBtnType: NavigationBtnType
+	
+	init(
+		isDisplayLeftBtn: Bool = true,
+		isDisplayRightBtn: Bool = true,
+		leftBtnAction: @escaping () -> Void = {},
+		rightBtnAction: @escaping () -> Void = {},
+		rightBtnType: NavigationBtnType = .edit
+	) {
+		self.isDisplayLeftBtn = isDisplayLeftBtn
+		self.isDisplayRightBtn = isDisplayRightBtn
+		self.leftBtnAction = leftBtnAction
+		self.rightBtnAction = rightBtnAction
+		self.rightBtnType = rightBtnType
+	}
+	
   var body: some View {
-    Text("CustomNavigationBar")
+		HStack {
+			if isDisplayLeftBtn {
+				Button {
+					leftBtnAction()					// 전달 받은 액션을 버튼에 대딥
+				} label: {
+					Image("leftArrow")
+				}
+				
+			}
+			
+			Spacer()
+			
+			if isDisplayRightBtn {
+				Button {
+				 rightBtnAction()
+				} label: {
+					if rightBtnType == .close {
+						Image("close")
+					} else {
+						Text(rightBtnType.rawValue)
+							.foregroundColor(.customBlack)
+					}
+				}
+			}
+		}
+		.padding(.horizontal, 20)
+		.frame(height: 20)
   }
+
 }
 
 struct CustomNavigationBar_Previews: PreviewProvider {
