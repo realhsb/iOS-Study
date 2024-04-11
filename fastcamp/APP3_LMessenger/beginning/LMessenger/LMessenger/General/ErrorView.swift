7ab4a14b1b9913a7b47357e4ba141dd2306394c0
realhsb <realhsb@gmail.com>
2024-04-11 21:44:00 +0900

    feat: MainView에서 통신 실패시 사용되는 뷰

---
+++ fastcamp/APP3_LMessenger/beginning/LMessenger/LMessenger/General/ErrorView.swift
@@ -0,0 +1,18 @@
+//
+//  ErrorView.swift
+//  LMessenger
+//
+//  Created by Subeen on 2/23/24.
+//
+
+import SwiftUI
+
+struct ErrorView: View {
+    var body: some View {
+        Text("Error View")
+    }
+}
+
+#Preview {
+    ErrorView()
+}
