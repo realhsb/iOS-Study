09c3d767329c616637733dba04f6bfab2f2eae06
realhsb <realhsb@gmail.com>
2024-04-11 21:45:19 +0900

    feat: MainView 통신 상태 분기 처리

---
+++ fastcamp/APP3_LMessenger/beginning/LMessenger/LMessenger/General/PlaceholderView.swift
@@ -0,0 +1,18 @@
+//
+//  PlaceholderView.swift
+//  LMessenger
+//
+//  Created by Subeen on 2/23/24.
+//
+
+import SwiftUI
+
+struct PlaceholderView: View {
+    var body: some View {
+        Color.bgWh
+    }
+}
+
+#Preview {
+    PlaceholderView()
+}
