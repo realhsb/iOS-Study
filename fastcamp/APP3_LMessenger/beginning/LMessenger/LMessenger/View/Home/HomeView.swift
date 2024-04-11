6c4a6e78f02dbd291332405115b643a4906bfe5a
realhsb <realhsb@gmail.com>
2024-04-11 21:44:20 +0900

    fix: HomeView UI 수정 및 통신 오류 해결

--- fastcamp/APP3_LMessenger/beginning/LMessenger/LMessenger/View/Home/HomeView.swift
+++ fastcamp/APP3_LMessenger/beginning/LMessenger/LMessenger/View/Home/HomeView.swift
@@ -12,53 +12,74 @@ struct HomeView: View {
     
     var body: some View {
         NavigationStack {
-            ScrollView {
-                profileView
-                    .padding(.bottom, 30)
-                searchButton
-                    .padding(.bottom, 24)
-                HStack {
-                    Text("친구")
-                        .font(.system(size: 14))
-                        .foregroundColor(.bkText)
-                    Spacer()
+            contentView
+        }
+    }
+    
+    @ViewBuilder
+    var contentView: some View {
+        switch viewModel.phase {
+        case .notRequested:
+            PlaceholderView()
+                .onAppear {
+                    viewModel.send(action: .load)
                 }
-                .padding(.horizontal, 30)
-                
-                // 친구 목록 
-                if viewModel.users.isEmpty { // 없을 때
-                    Spacer(minLength: 89)
-                    emptyView
-                } else {
-                    ForEach(viewModel.users, id: \.id) { user in
-                        HStack(spacing: 8) {
-                            Image("person")
-                                .resizable()
-                                .frame(width: 40, height: 40)
-                                .clipShape(Circle())
-                            Text(user.name)
-                                .font(.system(size: 12))
-                                .foregroundColor(.bkText)
-                            Spacer()
-                        }
-                        .padding(.horizontal, 30)
+        case .loading:
+            LoadingView()
+        case .success:
+            loadedView
+        case .fail:
+            ErrorView()
+        }
+    }
+    
+    var loadedView: some View {
+        ScrollView {
+            profileView
+                .padding(.bottom, 30)
+            searchButton
+                .padding(.bottom, 24)
+            HStack {
+                Text("친구")
+                    .font(.system(size: 14))
+                    .foregroundColor(.bkText)
+                Spacer()
+            }
+            .padding(.horizontal, 30)
+            
+            // 친구 목록
+            if viewModel.users.isEmpty { // 없을 때
+                Spacer(minLength: 89)
+                emptyView
+            } else {
+                ForEach(viewModel.users, id: \.id) { user in
+                    HStack(spacing: 8) {
+                        Image("person")
+                            .resizable()
+                            .frame(width: 40, height: 40)
+                            .clipShape(Circle())
+                        Text(user.name)
+                            .font(.system(size: 12))
+                            .foregroundColor(.bkText)
+                        Spacer()
                     }
+                    .padding(.horizontal, 30)
                 }
-            }
-            .toolbar {
-                Image("bookmark")
-                Image("notifications")
-                Image("person_add")
-                Button {
-                    // TODO:
-                } label: {
-                    Image("settings")
-                }
-            }
-            .onAppear {
-                viewModel.send(action: .load)
             }
         }
+        .toolbar {
+            Image("bookmark")
+            Image("notifications")
+            Image("person_add")
+            Button {
+                // TODO:
+            } label: {
+                Image("settings")
+            }
+        }
+//        .onAppear {
+//            viewModel.send(action: .load)
+//        }
     }
     
     var profileView: some View {
