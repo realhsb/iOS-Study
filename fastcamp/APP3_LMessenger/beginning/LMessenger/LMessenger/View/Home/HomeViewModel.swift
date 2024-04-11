1ce87bf477f5ba5595d381d7074c61bb7e58ccad
realhsb <realhsb@gmail.com>
2024-04-11 21:44:34 +0900

    fix: HomeViewModel 디버깅

--- fastcamp/APP3_LMessenger/beginning/LMessenger/LMessenger/View/Home/HomeViewModel.swift
+++ fastcamp/APP3_LMessenger/beginning/LMessenger/LMessenger/View/Home/HomeViewModel.swift
@@ -16,42 +16,53 @@ class HomeViewModel: ObservableObject {
     
     @Published var myUser: User?
     @Published var users: [User] = []
+    @Published var phase: Phase = .notRequested // 사용자 이름, 상태메세지
     
     private var userId: String
-    private var continer: DIContainer
+    
+    private var container: DIContainer
     private var subscriptions = Set<AnyCancellable>()
     
     init(container: DIContainer, userId: String) {
-        self.continer = container
+        self.container = container
         self.userId = userId
     }
     
-    func send(action: Action) {
+    func send(action: Action) {        
         switch action {
         case .load:  // getUser? ->  onAppear시 호출 / 친구목록도 onAppear시 불렸으면 ㅇㅇ
                     // getuser와 loadUser가 각각 호출돼도 괜찮지만, 한꺼번에 호출되는 것이 좋겠다! -> 두 스트림을 연결하여 호출
                     // getUser 호출 후, loadUser 호출
-            // TODO:
-            continer.services.userService.getUser(userId: userId)   // 친구 목록까지 잘 불러옴! 
+            phase = .loading
+            
+            container.services.userService.getUser(userId: userId)   // 친구 목록까지 잘 불러옴!
+                .handleEvents(receiveOutput: { [weak self] user in
+                    self?.myUser = user
+                    print("Hiii")
+                })
                 .handleEvents(receiveOutput: { [weak self] user in // 유저 정보 세팅
                     self?.myUser = user
+                    print("hi")
                 })  // 이 스트림의 사이드 이펙트. 이벤트 중간에 어떤 작업을 하고 싶을 때 사용
                 .flatMap { user in
-                    self.continer.services.userService.loadUsers(id: user.id)
+                    self.container.services.userService.loadUsers(id: user.id)
                 }
-                .sink { completion in
-                    //TODO:
+                .sink { [weak self] completion in
+                    if case .failure = completion {
+                        print("fail")
+                        self?.phase = .fail
+                    }
                 } receiveValue: { [weak self] users in
+                    print("success")
+                    
+                    self?.phase = .success
                     self?.users = users
+                    print("phase1 : \(self?.phase)")
                 }.store(in: &subscriptions)
-//                .sink { completion in
-//                    // TODO:
-//                } receiveValue: { [weak self] user in
-//                    self?.myUser = user
-//                }.store(in: &subscriptions)
-
-            return
+            print("phase2 : \(phase)")
+//            print(phase)
         }
     }
 }
+
 
