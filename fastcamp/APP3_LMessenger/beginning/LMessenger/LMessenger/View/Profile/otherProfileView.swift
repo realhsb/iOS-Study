bc8d4a430f2741df4b298ae2e68d1871a1ea20ec
realhsb <realhsb@gmail.com>
2024-04-15 22:57:38 +0900

    feat: 프로필뷰 추가

---
+++ fastcamp/APP3_LMessenger/beginning/LMessenger/LMessenger/View/Profile/otherProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  otherProfileView.swift
+//  LMessenger
+//
+//  Created by Subeen on 4/13/24.
+//
+
+import SwiftUI
+
+struct otherProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    otherProfileView()
+}
