5e7db4855927742c3c5015fa34226ae5870ff9ed
realhsb <realhsb@gmail.com>
2024-04-24 01:39:09 +0900

    design: 마이프로필 UI 구현

---
+++ fastcamp/APP3_LMessenger/beginning/LMessenger/LMessenger/View/Profile/MyProfileView.swift
@@ -0,0 +1,103 @@
+//
+//  myProfileView.swift
+//  LMessenger
+//
+//  Created by Subeen on 4/13/24.
+//
+
+import SwiftUI
+
+struct MyProfileView: View {
+    
+    @Environment(\.dismiss) var dismiss
+    @StateObject var viewModel: MyprofileViewModel
+    
+    var body: some View {
+        NavigationStack {
+            ZStack {
+                Image("profile_bg") // 기본적으로 이미지는 safe area 포함하지 않음
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .ignoresSafeArea(edges: .vertical)
+                
+                VStack(spacing: 0) {
+                    Spacer()
+                    
+                    profileView
+                        .padding(.bottom, 16)
+                    
+                    nameView
+                        .padding(.bottom, 26)
+                    
+                    descriptionView
+                    
+                    Spacer()
+                    
+                    menuView
+                        .padding(.bottom, 58)
+                }
+            }
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button {
+                        dismiss()
+                    } label: {
+                        Image("close")
+                    }
+                }
+            }
+        }
+    }
+    
+    var profileView: some View {
+        Button {
+            // TODO:
+        } label: {
+            Image("person")
+                .resizable()
+                .frame(width: 80, height: 80)
+                .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+        }
+    }
+    
+    var nameView: some View {
+        Text(viewModel.userInfo?.name ?? "이름")
+            .font(.system(size: 24, weight: .bold))
+//            .foregroundStyle()
+            .foregroundColor(.bgWh)
+    }
+    
+    var descriptionView: some View {
+        Button {
+            // TODO:
+        } label: {
+            Text(viewModel.userInfo?.description ?? "상태메시지를 입력해주세요.")
+                .font(.system(size: 14))
+                .foregroundColor(.bgWh)
+        }
+    }
+    
+    var menuView: some View {
+        HStack(alignment: .top, spacing: 27) {
+            // TODO:
+            ForEach(MyProfileMenuType.allCases, id: \.self) { menu in
+                Button {
+                } label: {
+                    VStack(alignment: .center) {
+                        Image(menu.imageName)
+                            .resizable()
+                            .frame(width: 50, height: 50)
+                        
+                        Text(menu.description)
+                            .font(.system(size: 10))
+                            .foregroundColor(.bgWh)
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    MyProfileView(viewModel: .init(container: DIContainer(services: StubService()), userId: "user1_id"))
+}
---
+++ fastcamp/APP3_LMessenger/beginning/LMessenger/LMessenger/View/Profile/OtherProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  otherProfileView.swift
+//  LMessenger
+//
+//  Created by Subeen on 4/13/24.
+//
+
+import SwiftUI
+
+struct OtherProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OtherProfileView()
+}
