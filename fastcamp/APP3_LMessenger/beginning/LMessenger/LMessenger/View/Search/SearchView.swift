7ae97bd8cfbd7a3526c5ea6ee4ba77f7386d5366
realhsb <realhsb@gmail.com>
2024-06-13 16:21:39 +0900

    feat: Search View

---
+++ fastcamp/APP3_LMessenger/beginning/LMessenger/LMessenger/View/Search/SearchView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchView.swift
+//  LMessenger
+//
+//  Created by Subeen on 6/9/24.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchView()
+}
