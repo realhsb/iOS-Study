649327457f7c1dcc324fc472a84726b22b02f4c1
realhsb <realhsb@gmail.com>
2024-06-13 19:09:37 +0900

    feat: 채팅 목록 뷰 및 뷰모델

--- fastcamp/APP3_LMessenger/beginning/LMessenger/LMessenger/View/ChatList/ChatListView.swift
+++ fastcamp/APP3_LMessenger/beginning/LMessenger/LMessenger/View/ChatList/ChatListView.swift
@@ -8,11 +8,65 @@
 import SwiftUI
 
 struct ChatListView: View {
+    
+    @EnvironmentObject var navigationRouter: NavigationRouter
+    @StateObject var viewModel: ChatListViewModel
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack(path: $navigationRouter.destinations) {
+            ScrollView {
+                NavigationLink(value: NavigationDestination.search) {
+                    SearchButton()
+                }
+                .padding(.top, 14)
+                .padding(.bottom, 14)
+                
+                ForEach(viewModel.chatRooms, id: \.self) { chatRoom in
+                    ChatRoomCell(chatRoom: chatRoom, userId: viewModel.userId)
+                }
+            }
+            .navigationTitle("대화")
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationDestination(for: NavigationDestination.self) {
+                NavigationRoutingView(destination: $0)
+            }
+            .onAppear {
+                viewModel.send(action: .load)
+            }
+        }
+    }
+}
+
+fileprivate struct ChatRoomCell: View {
+    let chatRoom: ChatRoom
+    let userId: String
+    
+    var body: some View {
+        NavigationLink(value: NavigationDestination.chat) {
+            HStack(spacing: 8) {
+                Image("person")
+                    .resizable()
+                    .frame(width: 40, height: 40)
+                VStack(alignment: .leading, spacing: 3) {
+                    Text(chatRoom.otherUserName)
+                        .font(.system(size: 14, weight: .bold))
+                        .foregroundColor(.bkText)
+                    
+                    if let lastMessage = chatRoom.lastMessage { // 마지막메세지가 있을 경우만
+                        Text(lastMessage)
+                            .font(.system(size: 12))
+                            .foregroundColor(.greyDeep)
+                    }
+                }
+                Spacer()
+            }
+            .padding(.horizontal, 30)
+            .padding(.bottom, 17)
+        }
     }
 }
 
 #Preview {
-    ChatListView()
+    ChatListView(viewModel: .init(container: DIContainer(services: StubService()), userId: "user1_id"))
+        .environmentObject(NavigationRouter())
 }
