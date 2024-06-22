8403e7f218d31a73aa67c1657c276f5ddf152957
realhsb <realhsb@gmail.com>
2024-06-22 20:15:20 +0900

    feat: 채팅방 모델

---
+++ fastcamp/APP3_LMessenger/beginning/LMessenger/LMessenger/Model/ChatData.swift
@@ -0,0 +1,13 @@
+//
+//  ChatData.swift
+//  LMessenger
+//
+//  Created by Subeen on 6/22/24.
+//
+
+import Foundation
+
+struct ChatData: Hashable {
+    var dateStr: String
+    var chats: [Chat]
+}
