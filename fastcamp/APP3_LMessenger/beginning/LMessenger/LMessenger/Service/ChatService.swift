6ad52379b72e51d8e5795d0fd5a60ca6578f9a9c
realhsb <realhsb@gmail.com>
2024-06-29 22:22:48 +0900

    feat: Chat Service 생성 및 연결

---
+++ fastcamp/APP3_LMessenger/beginning/LMessenger/LMessenger/Service/ChatService.swift
@@ -0,0 +1,37 @@
+//
+//  ChatService.swift
+//  LMessenger
+//
+//  Created by Subeen on 6/29/24.
+//
+
+import Foundation
+import Combine
+
+protocol ChatServiceType {
+    func addChat(_ chat: Chat, to chatRoomId: String) -> AnyPublisher<Chat, ServiceError>
+}
+
+class ChatService: ChatServiceType {
+    
+    private let dbRepository: ChatDBRepositoryType
+    
+    init(dbRepository: ChatDBRepositoryType) {
+        self.dbRepository = dbRepository
+    }
+    
+    func addChat(_ chat: Chat, to chatRoomId: String) -> AnyPublisher<Chat, ServiceError> {
+        // TODO: 채팅 Id는 realtime database에서 제공하는 childByAutoId 는 타임스탬프 기반. 메시지를 시간순으로 자동 정렬 -> DBRepository에서 작업! 
+        
+        return dbRepository.addChat(chat.toObject(), to: chatRoomId)
+            .map { chat }
+            .mapError { ServiceError.error($0) }
+            .eraseToAnyPublisher()
+    }
+}
+
+class StubChatService: ChatServiceType {
+    func addChat(_ chat: Chat, to chatRoomId: String) -> AnyPublisher<Chat, ServiceError> {
+        Empty().eraseToAnyPublisher()
+    }
+}
