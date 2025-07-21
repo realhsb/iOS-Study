641b82d5a8011e901098a273b1d37df8b1528926
숩 <realhsb@gmail.com>
2025-07-22 00:17:40 +0900

    feat: MVI 패턴 적용

--- fastcamp/APP4_CCommerce/APP4_CCommerce/Feature/Home/HomeViewController.swift
+++ fastcamp/APP4_CCommerce/APP4_CCommerce/Feature/Home/HomeViewController.swift
@@ -9,125 +9,116 @@ import UIKit
 import Combine
 
 class HomeViewController: UIViewController {
-  
-  enum Section: Int {
-    case banner
-    case horizontalProductItem
-    case verticalProductItem
-  }
-  
-  @IBOutlet weak var collectionView: UICollectionView!
-  
-  private var dataSource: UICollectionViewDiffableDataSource<Section, AnyHashable>?
-  private var compositionalLayout: UICollectionViewCompositionalLayout = setCompositionalLayout()
-  private var viewModel: HomeViewModel = HomeViewModel()
-  private var cancellables: Set<AnyCancellable> = []
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
     
-    bindingViewModel()
-    viewModel.loadData()
-    setDataSource()
-    collectionView.collectionViewLayout = compositionalLayout
-  }
-  
-  private static func setCompositionalLayout() -> UICollectionViewCompositionalLayout { // 초기화할 때 사용할 수 있도록 static으로 선언
-    UICollectionViewCompositionalLayout { section, _ in
-      switch Section(rawValue: section) {
-      case .banner:
-        return HomeBannerCollectionViewCell.bannerLayout()
-        
-      case .horizontalProductItem:
-        return HomeProductCollectionViewCell.horizontalProductItemLayout()
-        
-      case .verticalProductItem:
-        return HomeProductCollectionViewCell.verticalProductItemLayout()
-        
-      case .none: return nil
-      }
+    enum Section: Int {
+        case banner
+        case horizontalProductItem
+        case verticalProductItem
     }
-  }
-  
-  private func bindingViewModel() {
-    // $bannerViewModels -> 달러표시는 관찰할 수 있는 친구
-    // DispatchQueue.main 에서 진행
-    // 홈뷰컨이 살아있는 동안은 계속 관찰 cancellables이 사라지면 이 관찰도 끝 ... cancellables은 홈뷰컨이 사라지면 사라짐
-    viewModel.$bannerViewModels.receive(on: DispatchQueue.main)
-      .sink { [weak self] _ in
-        self?.applySnapshot()
-      }.store(in: &cancellables)
     
-    viewModel.$horizontalProductViewModels.receive(on: DispatchQueue.main)
-      .sink { [weak self] _ in
-        self?.applySnapshot()
-      }
+    @IBOutlet weak var collectionView: UICollectionView!
     
-    viewModel.$verticalProductViewModels.receive(on: DispatchQueue.main)
-      .sink { [weak self] _ in
-        self?.applySnapshot()
-      }
-  }
-  
-  private func setDataSource() {
-    dataSource = UICollectionViewDiffableDataSource(collectionView: collectionView, cellProvider: { [weak self]  collectionView, indexPath, viewModel in
-      
-      switch Section(rawValue: indexPath.section) {
+    private var dataSource: UICollectionViewDiffableDataSource<Section, AnyHashable>?
+    private var compositionalLayout: UICollectionViewCompositionalLayout = setCompositionalLayout()
+    private var viewModel: HomeViewModel = HomeViewModel()
+    private var cancellables: Set<AnyCancellable> = []  // 바인딩한 걸 관찰하기 위한
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
-      case .banner:
-        return self?.bannerCell(collectionView, indexPath, viewModel)
+        bindingViewModel()        // 1️⃣ 값을 관찰할 수 있도록 세팅
+        viewModel.process(action: .loadData)      // 2️⃣ 데이터 가져오기
+        setDataSource()           // 3️⃣ 데이터 소스 세팅
+        collectionView.collectionViewLayout = compositionalLayout
+    }
+    
+    private static func setCompositionalLayout() -> UICollectionViewCompositionalLayout { // 초기화할 때 사용할 수 있도록 static으로 선언
+        UICollectionViewCompositionalLayout { section, _ in
+            switch Section(rawValue: section) {
+            case .banner:
+                return HomeBannerCollectionViewCell.bannerLayout()
+                
+            case .horizontalProductItem:
+                return HomeProductCollectionViewCell.horizontalProductItemLayout()
+                
+            case .verticalProductItem:
+                return HomeProductCollectionViewCell.verticalProductItemLayout()
+                
+            case .none: return nil
+            }
+        }
+    }
+    
+    private func bindingViewModel() { // 1. 값을 관찰
+        // $bannerViewModels -> 달러표시는 관찰할 수 있는 친구
+        // DispatchQueue.main 에서 진행
+        // 홈뷰컨이 살아있는 동안은 계속 관찰 cancellables이 사라지면 이 관찰도 끝 ... cancellables은 홈뷰컨이 사라지면 사라짐
+        viewModel.state.$collectionViewModels.receive(on: DispatchQueue.main)
+            .sink { [weak self] _ in  // 2. 값이 발생하면 sink가 됨. 각각의 변수 업데이트
+                self?.applySnapshot()   // 3. 스냅샷 호출
+            }.store(in: &cancellables)
         
-      case .horizontalProductItem, .verticalProductItem:
-        return self?.productItemCell(collectionView, indexPath, viewModel)
-        
-        
-      case .none:
-        
-        return .init()
-      }
-    })
-  }
-  
+    }
+    
+    private func setDataSource() {
+        dataSource = UICollectionViewDiffableDataSource(collectionView: collectionView, cellProvider: { [weak self]  collectionView, indexPath, viewModel in
+            
+            switch Section(rawValue: indexPath.section) {
+                
+            case .banner:
+                return self?.bannerCell(collectionView, indexPath, viewModel)
+                
+            case .horizontalProductItem, .verticalProductItem:
+                return self?.productItemCell(collectionView, indexPath, viewModel)
+                
+                
+            case .none:
+                
+                return .init()
+            }
+        })
+    }
+    
     private func applySnapshot() {
+        
+        var snapShot: NSDiffableDataSourceSnapshot<Section, AnyHashable> = NSDiffableDataSourceSnapshot<Section, AnyHashable>()
+        
+        if let bannerViewModels = viewModel.state.collectionViewModels.bannerViewModels {  // 4. 뷰모델에 해당 값이 있으면
+            snapShot.appendSections([.banner])                    // 5. 해당 섹션 생성
+            snapShot.appendItems(bannerViewModels, toSection: .banner)  // 6. 섹션에 아이템 추가
+        }
+        
+        if let horizontalProductViewModels = viewModel.state.collectionViewModels.horizontalProductViewModels {
+            snapShot.appendSections([.horizontalProductItem])
+            snapShot
+                .appendItems(horizontalProductViewModels, toSection: .horizontalProductItem)
+        }
+        
+        if let verticalProductViewModels = viewModel.state.collectionViewModels.verticalProductViewModels {
+            snapShot.appendSections([.verticalProductItem])
+            snapShot
+                .appendItems(verticalProductViewModels, toSection: .verticalProductItem)
+        }
+        
+        dataSource?.apply(snapShot) // 7. 스냅샷 업데이트
+    }
     
-    var snapShot: NSDiffableDataSourceSnapshot<Section, AnyHashable> = NSDiffableDataSourceSnapshot<Section, AnyHashable>()
+    private func bannerCell(_ collectionView: UICollectionView, _ indexPath: IndexPath, _ viewModel: AnyHashable) -> UICollectionViewCell {
+        guard let viewModel = viewModel as? HomeBannerCollectionViewCellViewModel,
+              let cell: HomeBannerCollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "HomeBannerCollectionViewCell", for: indexPath) as? HomeBannerCollectionViewCell else { return .init() }
+        
+        cell.setViewModel(viewModel)
+        return cell
+    }
     
-      if let bannerViewModels = viewModel.bannerViewModels {  // 데이터가 있을 때만 아이템에 데이터 넣어주기
-        snapShot.appendSections([.banner])
-        snapShot.appendItems(bannerViewModels, toSection: .banner)
-      }
-    
-      if let horizontalProductViewModels = viewModel.horizontalProductViewModels {
-        snapShot.appendSections([.horizontalProductItem])
-        snapShot
-          .appendItems(horizontalProductViewModels, toSection: .horizontalProductItem)
-      }
-      
-      if let verticalProductViewModels = viewModel.verticalProductViewModels {
-        snapShot.appendSections([.verticalProductItem])
-        snapShot
-          .appendItems(verticalProductViewModels, toSection: .verticalProductItem)
-      }
-    
-    dataSource?.apply(snapShot)
-  }
-  
-  private func bannerCell(_ collectionView: UICollectionView, _ indexPath: IndexPath, _ viewModel: AnyHashable) -> UICollectionViewCell {
-    guard let viewModel = viewModel as? HomeBannerCollectionViewCellViewModel,
-          let cell: HomeBannerCollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "HomeBannerCollectionViewCell", for: indexPath) as? HomeBannerCollectionViewCell else { return .init() }
-    
-    cell.setViewModel(viewModel)
-    return cell
-  }
-  
-  private func productItemCell(_ collectionView: UICollectionView, _ indexPath: IndexPath, _ viewModel: AnyHashable) -> UICollectionViewCell {
-    guard let viewModel = viewModel as? HomeProductCollectionViewCellViewModel,
-          let cell: HomeProductCollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "HomeProductCollectionViewCell", for: indexPath) as? HomeProductCollectionViewCell else { return .init() }
-    cell.setViewModel(viewModel)
-    return cell
-  }
+    private func productItemCell(_ collectionView: UICollectionView, _ indexPath: IndexPath, _ viewModel: AnyHashable) -> UICollectionViewCell {
+        guard let viewModel = viewModel as? HomeProductCollectionViewCellViewModel,
+              let cell: HomeProductCollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "HomeProductCollectionViewCell", for: indexPath) as? HomeProductCollectionViewCell else { return .init() }
+        cell.setViewModel(viewModel)
+        return cell
+    }
 }
 
 #Preview {
-  UIStoryboard(name: "Home", bundle: nil).instantiateInitialViewController() as! HomeViewController
+    UIStoryboard(name: "Home", bundle: nil).instantiateInitialViewController() as! HomeViewController
 }
--- fastcamp/APP4_CCommerce/APP4_CCommerce/Feature/Home/HomeViewModel.swift
+++ fastcamp/APP4_CCommerce/APP4_CCommerce/Feature/Home/HomeViewModel.swift
@@ -9,45 +9,94 @@ import Combine
 import Foundation
 
 class HomeViewModel {
-  
-  @Published var bannerViewModels: [HomeBannerCollectionViewCellViewModel]?
-  @Published var horizontalProductViewModels: [HomeProductCollectionViewCellViewModel]?
-  @Published var verticalProductViewModels: [HomeProductCollectionViewCellViewModel]?
-  
-  private var loadDataTask: Task<Void, Never>?
-  
-  func loadData() {
-    Task {  // 비동기, 뷰모델이 없어져도 task는 마저 동작하고 deinit이 호출될 수 있음.
-      do {
-        let response = try await NetworkService.shared.getHomeData() // AsyncThrough error 반환
-        let bannerViewModels = response.banners.map {
-          HomeBannerCollectionViewCellViewModel(bannerImageUrl: $0.imageUrl)
-        }
-        let horizontalProductViewModels = response.horizontalProducts.map {
-          HomeProductCollectionViewCellViewModel(imageUrlString: $0.imageUrl,
-                                                 title: $0.title,
-                                                 reasonDiscountString: $0.discount,
-                                                 originalPrice: "\($0.originalPrice)",
-                                                 discountPrice: "\($0.discountPrice)")
-        }
-        let verticalProductViewModels = response.verticalProducts.map {
-          HomeProductCollectionViewCellViewModel(imageUrlString: $0.imageUrl,
-                                                 title: $0.title,
-                                                 reasonDiscountString: $0.discount,
-                                                 originalPrice: "\($0.originalPrice)",
-                                                 discountPrice: "\($0.discountPrice)")
-        }
-        self.bannerViewModels = bannerViewModels
-        self.horizontalProductViewModels = horizontalProductViewModels
-        self.verticalProductViewModels = verticalProductViewModels
-        
-      } catch {
-        print("network error: \(error)")
-      }
+    
+    enum Action {
+        case loadData
+        case getDateSuccess(HomeResponse)
+        case getDataFailure(Error)
     }
-  }
-  
-  deinit {  // 뷰모델이 사라져야할 때 비동기 진행을 취소시킴
-    loadDataTask?.cancel()
-  }
+    
+    final class State {
+        struct CollectionViewModels {
+            
+            // 값을 받고, UI를 업데이트 해야 함 -> 메인스레드에서 동작 해야 함
+            // 함수 실행을 끝냈을 때 메인 액터에서 동작하도록 함.
+            
+            // 2. 여기있는 상태가 업데이트
+            var bannerViewModels: [HomeBannerCollectionViewCellViewModel]?
+            var horizontalProductViewModels: [HomeProductCollectionViewCellViewModel]?
+            var verticalProductViewModels: [HomeProductCollectionViewCellViewModel]?
+        }
+        @Published var collectionViewModels: CollectionViewModels = CollectionViewModels()  // 1. 얘를 변경하면
+    }
+    private(set) var state: State = State()
+    private var loadDataTask: Task<Void, Never>?
+    
+    func process(action: Action) {
+        switch action {
+        case .loadData:
+            loadData()
+            
+        case let .getDateSuccess(response): // 조회 성공 시, response 넘겨줌
+            transformResponse(response)
+             
+        case let .getDataFailure(error):
+            print("network error: \(error)")
+        }
+    }
+    
+    private func loadData() {
+        Task {  // 비동기, 뷰모델이 없어져도 task는 마저 동작하고 deinit이 호출될 수 있음.
+            do {
+
+                let response = try await NetworkService.shared.getHomeData() // AsyncThrough error 반환
+                process(action: .getDateSuccess(response))
+                // 각각 동작. 병렬적으로 동작.
+
+            } catch {
+                process(action: .getDataFailure(error))
+                
+            }
+        }
+    }
+    
+    private func transformResponse(_ response: HomeResponse) {
+        Task { await transformBanner(response) }  // 값을 받아와서 bannerViewModels에 세팅
+        Task { await transformHorizontalProduct(response) }
+        Task { await transformVerticalProduct(response) }
+    }
+    
+    // async 붙인 이유? 메인 액터도 async하게 동작하는 걸 보장
+    @MainActor
+    private func transformBanner(_ response: HomeResponse) async {
+        state.collectionViewModels.bannerViewModels = response.banners.map {
+            HomeBannerCollectionViewCellViewModel(bannerImageUrl: $0.imageUrl)
+        }
+    }
+    
+    @MainActor
+    private func transformHorizontalProduct(_ response: HomeResponse) async {
+        state.collectionViewModels.horizontalProductViewModels = response.horizontalProducts.map {
+            HomeProductCollectionViewCellViewModel(imageUrlString: $0.imageUrl,
+                                                   title: $0.title,
+                                                   reasonDiscountString: $0.discount,
+                                                   originalPrice: "\($0.originalPrice)",
+                                                   discountPrice: "\($0.discountPrice)")
+        }
+    }
+    
+    @MainActor
+    private func transformVerticalProduct(_ response: HomeResponse) async {
+        state.collectionViewModels.verticalProductViewModels = response.verticalProducts.map {
+            HomeProductCollectionViewCellViewModel(imageUrlString: $0.imageUrl,
+                                                   title: $0.title,
+                                                   reasonDiscountString: $0.discount,
+                                                   originalPrice: "\($0.originalPrice)",
+                                                   discountPrice: "\($0.discountPrice)")
+        }
+    }
+    
+    deinit {  // 뷰모델이 사라져야할 때 비동기 진행을 취소시킴
+        loadDataTask?.cancel()
+    }
 }
