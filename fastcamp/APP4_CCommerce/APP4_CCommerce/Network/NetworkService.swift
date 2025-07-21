59103e47b73de8de282b83d6ca9dc5134f944887
숩 <realhsb@gmail.com>
2025-07-22 01:23:58 +0900

    feat: network 코드 분리

--- fastcamp/APP4_CCommerce/APP4_CCommerce/Network/NetworkService.swift
+++ fastcamp/APP4_CCommerce/APP4_CCommerce/Network/NetworkService.swift
@@ -8,15 +8,26 @@
 import Foundation
 
 class NetworkService {
-  static let shared: NetworkService = NetworkService()
-  
-  func getHomeData() async throws -> HomeResponse {
-    let urlString =  "https://my-json-server.typicode.com/JeaSungLee/JsonAPIFastCampus/db"
-    guard let url = URL(string: urlString) else { throw URLError(.badURL) }
+    static let shared: NetworkService = NetworkService()
+     
+    private let hostURL = "https://my-json-server.typicode.com/JeaSungLee/JsonAPIFastCampus/"
     
-    let (data, response) = try await URLSession.shared.data(from: url)
-    guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else { throw URLError(.badServerResponse) }
-    let decodeData = try JSONDecoder().decode(HomeResponse.self, from: data)
-    return decodeData
-  }
+    private func createURL(withPath path: String) throws -> URL {
+        let urlString: String = "\(hostURL)\(path)"
+        guard let url = URL(string: urlString) else { throw URLError(.badURL) } // throw랑 throws 차이가 뭐지
+        return url
+    }
+    
+    private func fetchData(form url: URL) async throws -> Data {
+        let (data, response) = try await URLSession.shared.data(from: url)
+        guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else { throw URLError(.badServerResponse) }
+        return data
+    }
+    
+    func getHomeData() async throws -> HomeResponse {
+        let url = try createURL(withPath: "/db")
+        let data = try await fetchData(form: url)
+        let decodeData = try JSONDecoder().decode(HomeResponse.self, from: data)
+        return decodeData
+    }
 }
